9fdfd7801667b79ea29d64862ac9a1fb63c3857a
Pablo O <ortegauriol@gmail.com>
2020-10-18 16:59:26 +1300

    PushButton

--- Arduino Code/Button_Ser_write/Button_Ser_write.ino
+++ Arduino Code/Button_Ser_write/Button_Ser_write.ino
@@ -1,15 +1,14 @@
 // digital pin 2 has a pushbutton attached to it. Give it a name:
-int pushButton = 1;
+const int pushButton = 1;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+  // initialize serial communication at 9600 bits per second, 960 characters per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
   pinMode(pushButton, INPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   // read the input pin:
   int buttonState = digitalRead(pushButton);

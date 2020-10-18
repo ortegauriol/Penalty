2259976cafb5da1df6c1a6de24ad7bb8d51d0821
Pablo O <ortegauriol@gmail.com>
2020-10-18 22:14:16 +1300

    PushButton, FutbolPy and Bycicle example

--- Python/Futbol.py
+++ Python/Futbol.py
@@ -11,7 +11,7 @@ import numpy as np  # whole numpy lib is available, prepend 'np.'
 import os  # handy system and path functions
 import sys  # to get file system encoding
 import serial  #connecting to the serial port (Arduino)
-
+import math
 import copy
 import random
 
@@ -22,8 +22,8 @@ os.chdir(_thisDir)
 
 #OPEN THE SERIAL PORT TO COMMUNICATE WITH ARDUINO
 
-# ser = serial.Serial('COM3', 9600, timeout=0)
-# line = ser.readline()
+ser = serial.Serial('COM3', 9600, timeout=0)
+line = ser.readline()
 
 
 #Participant information
@@ -39,9 +39,52 @@ expInfo['date'] = data.getDateStr()
 
 # Experiment parameters
 
-taskInfo_brief={'Count down':True,'Trial by trial feedback':True, 'Step size (ms)':33, 'Stop limit (ms)':150,'Lower limit (ms)':500,
-                'n_go_trials (per block)':2, 'n_stop_trials (per block)':4,'n blocks':4, 'practice trials':False, 'n practice go trials':1,
-                'n practice stop trials':1, 'Full Screen':True, 'Total bar height':15, 'Threat Mode':True}
+taskInfo_brief={'n_go_trials (per block)':2, 'n_stop_trials (per block)':4,'n blocks':4, 'practice trials':False, 'n practice go trials':1,
+                'n practice stop trials':1, 'Full Screen':True, 'Pressure Mode':True}
 
 
-# Display the goal an goakeeper
+if not expInfo['Would you like to run this task using its default parameters?']:
+    dlg=gui.DlgFromDict(dictionary=taskInfo_brief, title='Experiment Parameters',
+        tip={
+        '% StopS':'# of trials that are Stops',
+        'n trials':'Total number of main trials',
+        'Step size (ms)':'If participant fails to stop the next Stop will be this much earlier in ms (i.e. staircase step size)', 
+        'Stop limit (ms)':'What is the closest to the target the StopS trial can be?'
+        # 'N Blocks':'How many blocks to perfom'
+        })
+    if dlg.OK ==False: 
+        ser.close()
+        core.quit()
+
+# Block and trial types
+trials=[0]*taskInfo_brief['n_stop_trials (per block)']+[1]*taskInfo_brief['n_go_trials (per block)']
+print('Type of trials (0 = Stop):', trials)
+
+if taskInfo_brief['Pressure Mode']:
+    if taskInfo_brief['n blocks']% 2 == 0:
+        blocks = [0]*int(taskInfo_brief['n blocks']/2) + [1]*int(taskInfo_brief['n blocks']/2)
+    else:
+        blocks = [1]*int(math.ceil(taskInfo_brief['n blocks']/2)) + [0]*int(math.floor(taskInfo_brief['n blocks']/2))
+else:
+    blocks = [0]*int(taskInfo_brief['n blocks'])
+print ('Block type (0 is non-pressure): ')
+#Could be a good idea to have a goal with public behind for under pressure
+np.random.shuffle(trials)
+block_trials = trials
+    
+# Random blocks
+np.random.shuffle(blocks)
+print (blocks)
+
+
+
+# Display the goal an goalkeeper
+
+
+
+while True :
+    print (line)
+
+
+
+

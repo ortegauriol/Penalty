09a006ef17b9972e12da4000c79afaf32ba3f3a7
Pablo O <ortegauriol@gmail.com>
2020-10-18 17:44:39 +1300

    PushButton, FutbolPy and Bycicle example

--- Arduino Code/Button_Ser_write/Button_Ser_write.ino
+++ Arduino Code/Button_Ser_write/Button_Ser_write.ino
@@ -1,19 +1,19 @@
+// Digital read and write with a push button to test things
+
 // digital pin 2 has a pushbutton attached to it. Give it a name:
 const int pushButton = 1;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second, 960 characters per second:
-  Serial.begin(9600);
-  // make the pushbutton's pin an input:
-  pinMode(pushButton, INPUT);
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second, 960 characters per second:
+  
+  pinMode(pushButton, INPUT); // make the pushbutton's pin an input:
 }
 
 void loop() {
   // read the input pin:
-  int buttonState = digitalRead(pushButton);
+  int buttonState = digitalRead(pushButton); //read the input pin
   
-  // print out the state of the button:
-  Serial.println(buttonState);
-  delay(2);        // delay in between reads for stability
+  Serial.println(buttonState);// print out the state of the button:
+  delay(1);        // delay in between reads for stability
 }
